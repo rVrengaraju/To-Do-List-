8b2f6fdb8e25b999fbb7987aed67a70b33fdadf4
Ruban Rengaraju <rubanrengaraju005@gmail.com>
2018-06-23 16:53:08 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ToDo from "./ToDo";
+
+var dest = document.querySelector("#root");
+
+
+ReactDOM.render(
+	<div>
+		<ToDo/>
+	</div>,
+	dest
+	);
